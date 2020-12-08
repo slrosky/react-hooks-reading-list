31edeba68826708e548f6e70defc0f334b17ae3a
Sarah Rosky <sarahrosky@gmail.com>
2020-12-08 13:24:33 -0800

    add back in index.js

--- book-list/src/App.js
+++ book-list/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import BookContextProvider from './contexts/BookContext';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
-
+        <Navbar />
       </BookContextProvider>
     </div>
   );
